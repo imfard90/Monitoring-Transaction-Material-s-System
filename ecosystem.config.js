8b326986bf0910b13b84c6d3c5a137ee070d2c7d
Imam Fardi <imfard90@gmail.com>
2026-09-24 00:57:47 +0700

    setting

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,19 +2,19 @@ module.exports = {
   apps: [
     {
       name: 'mtms-app',           // Nama aplikasi di dalam PM2
-      script: 'npm',
+      script: 'pnpm',
       args: 'start',
-      instances: 1,               // Jumlah instance (bisa diubah ke 'max' untuk memanfaatkan semua core CPU)
-      exec_mode: 'fork',          // Mode eksekusi ('fork' atau 'cluster')
+      instances: max,               // Jumlah instance (bisa diubah ke 'max' untuk memanfaatkan semua core CPU)
+      exec_mode: 'cluster',          // Mode eksekusi ('fork' atau 'cluster')
       autorestart: true,          // Restart otomatis jika aplikasi crash
       watch: false,               // Jangan watch perubahan file di production
-      max_memory_restart: '1G',   // Restart jika penggunaan RAM melebihi 1 GB
+      max_memory_restart: '8G',   // Restart jika penggunaan RAM melebihi 1 GB
       env: {
         NODE_ENV: 'development',
       },
       env_production: {
         NODE_ENV: 'production',
-        PORT: 3000,               // Port yang akan digunakan oleh aplikasi Next.js
+        PORT: 5000,               // Port yang akan digunakan oleh aplikasi Next.js
       }
     }
   ]
